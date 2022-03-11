53b6c4536008b824fdd99d7161fdf05989397acc
Marcos Laignier <32346579+MarcosLaignier@users.noreply.github.com>
2022-03-10 21:18:35 -0300

    Version01.1

---
+++ scripts.js
@@ -0,0 +1,14 @@
+/* var extendleftBar = document.getElementById('button');
+extendleftBar.onclick = function() {
+    if  (document.getElementById('extend_left_bar').style.display = 'block'){
+    document.getElementById('extend_left_bar').style.display = 'none';
+}
+}  */
+
+function toogle(){
+    if  (document.getElementById('extend_left_bar').style.display = 'none'){
+        document.getElementById('extend_left_bar').style.display = 'block';
+    }else
+        document.getElementById('extend_left_bar').style.display = 'none'
+    
+}
