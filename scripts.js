d430c9e3c0d2b3fbdd2d2fdb022c4e6c66fa7eda
Marcos Laignier <32346579+MarcosLaignier@users.noreply.github.com>
2022-03-21 20:04:52 -0300

    Version 01.06 Iniciando refatoracao e inclusao do JS

--- scripts.js
+++ scripts.js
@@ -5,10 +5,26 @@ extendleftBar.onclick = function() {
 }
 }  */
 
-function toogle(){
-    if  (document.getElementById('extend_left_bar').style.display == 'none'){
-        document.getElementById('extend_left_bar').style.display = 'block';
-    }else
-        document.getElementById('extend_left_bar').style.display = 'none'
-    
+function toogle() {
+  if (document.getElementById("extend_left_bar").style.display == "none") {
+    document.getElementById("extend_left_bar").style.display = "block";
+  } else document.getElementById("extend_left_bar").style.display = "none";
+}
+
+function validaCadCemiterio() {
+  if (document.getElementById("name_Cemiterio").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("endereco_Cemiterio").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("city_Cemiterio").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("state_Cemiterio").value == "") {
+    document.getElementById("alert_Form").style.display = "block";
+  } else if (document.getElementById("responsavel_Cemiterio").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else
+  {
+    document.getElementById("alert_Form_Error").style.display = "none";
+    document.getElementById("alert_Form_Sucess").style.display = "block";
+  }
 }
