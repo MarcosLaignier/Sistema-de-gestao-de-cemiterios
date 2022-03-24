ae42aa1862df3c34daac1a9300e1bc8cc14dbbb2
Marcos Laignier <32346579+MarcosLaignier@users.noreply.github.com>
2022-03-23 21:14:16 -0300

    Version 02.00 Iniciando refatoracao e inclusao do JS

--- scripts.js
+++ scripts.js
@@ -11,6 +11,8 @@ function toogle() {
   } else document.getElementById("extend_left_bar").style.display = "none";
 }
 
+
+/* Validacoes formularios */
 function validaCadCemiterio() {
   if (document.getElementById("name_Cemiterio").value == "") {
     document.getElementById("alert_Form_Error").style.display = "block";
@@ -26,5 +28,117 @@ function validaCadCemiterio() {
   {
     document.getElementById("alert_Form_Error").style.display = "none";
     document.getElementById("alert_Form_Sucess").style.display = "block";
+    /* document.getElementById('form_CCemiterio').reset(); */
+        
+    
+    
   }
 }
+
+function validaCadSepultura() {
+  if (document.getElementById("name_Sepultura").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("name_Cemiterio").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  }else
+  {
+    document.getElementById("alert_Form_Error").style.display = "none";
+    document.getElementById("alert_Form_Sucess").style.display = "block";
+    /* document.getElementById('form_CCemiterio').reset(); */
+        
+    
+    
+  }
+}
+
+function validaCadFalecido() {
+  if (document.getElementById("name_Falecido").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("cpf_Falecido").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("sexo_Falecido").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("nascimento_Falecido").value == "") {
+    document.getElementById("alert_Form").style.display = "block";
+  } else if (document.getElementById("naturalidade_Falecido").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("date_Falecimento").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else
+  {
+    document.getElementById("alert_Form_Error").style.display = "none";
+    document.getElementById("alert_Form_Sucess").style.display = "block";
+    /* document.getElementById('form_CCemiterio').reset(); */
+        
+    
+    
+  }
+}
+
+function validaCadFuneraria() {
+  if (document.getElementById("name_Funeraria").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("city_Funeraria").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("endereco_Funeraria").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("number_Funeraria").value == "") {
+    document.getElementById("alert_Form").style.display = "block";
+  }else
+  {
+    document.getElementById("alert_Form_Error").style.display = "none";
+    document.getElementById("alert_Form_Sucess").style.display = "block";
+    /* document.getElementById('form_CCemiterio').reset(); */
+        
+    
+    
+  }
+}
+
+
+function validaCadSepultamento() {
+  if (document.getElementById("name_Pessoa").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("cpf_Pessoa").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("name_Funeraria").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("date_Sepultamento").value == "") {
+    document.getElementById("alert_Form").style.display = "block";
+  } else if (document.getElementById("name_Cemiterio").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else if (document.getElementById("date_Falecimento").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  }else if (document.getElementById("name_Sepultura").value == "") {
+    document.getElementById("alert_Form_Error").style.display = "block";
+  } else
+  {
+    document.getElementById("alert_Form_Error").style.display = "none";
+    document.getElementById("alert_Form_Sucess").style.display = "block";
+    /* document.getElementById('form_CCemiterio').reset(); */
+        
+    
+    
+  }
+}
+
+function DadosCadCemiterio() {
+  var name_Cemiterio = document.getElementById("name_Cemiterio")
+  var name_Cemiterio = document.getElementById("endereco_Cemiterio")
+  var name_Cemiterio = document.getElementById("city_Cemiterio")
+  var name_Cemiterio = document.getElementById("state_Cemiterio")
+  var name_Cemiterio = document.getElementById("responsavel_Cemiterio")
+
+    
+  }
+
+  function DadosCadCemiterio2() {
+
+  var itens = document.getElementsByTagName('input');
+  for (i=0; i<itens.length; i++){ 
+    itens[i].getAttribute("alt") + ": " + itens[i].getAttribute("value");
+    }
+
+    console.log(itens).value
+  }
+  
